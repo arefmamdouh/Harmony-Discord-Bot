a8e1d6f114b5a84c1af528748e0ca673b8707011
arefmamdouh <amamdouh@aucegypt.edu>
2022-04-23 02:03:04 +0200

    just a feature to the help

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,8 @@ module.exports = {
     execute(message, args, Discord){
             const newEmbed = new Discord.MessageEmbed()
             .setColor('#f01347')
-            .setDescription('An embed for the list of commands used by Harmony')
-            .setTitle('Commands')
             .addFields(
-                {name: `the prefix is harmony,`, 
+                {name: `Commands`, 
                 value: 
                 "`harmony, hi                  : `I say hello back :D I might say something weird though, you can also say hello. \n" +
                 "`harmony, play <music>        : `After using this command type in whatever song you want to listen to. Not just music btw you can also ask for a sound or a podcast. \n" +
@@ -16,7 +14,8 @@ module.exports = {
                 "`harmony, ping                : `I\'ll leave it as a surprise.\n" +
                 "`harmony, creator             : `I\'ll tell you about the man that made me.\n" +
                 "`harmony, prayer <prayer name>: `Use harmony, prayer followed by the name of the prayer.\n" +
-                "`prayer names                 : `fajr, sunrise, dhuhr, asr, maghrib, and isha."}
+                "`prayer names                 : `fajr, sunrise, dhuhr, asr, maghrib, and isha." +
+                "Also when i say <music> for example you type the name of the song/podcast without the <>."}
             )
             .setFooter('Have fun homies. With love, Aref Mamdouh');
 
